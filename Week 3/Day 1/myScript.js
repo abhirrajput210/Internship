f10d13f859abe270a7966319fd86ce798895e597
abhirrajput210 <abhishekrajput.lamprostech@gmail.com>
2023-02-16 12:18:27 +0530

    Fetch Example

--- Week 3/Day 1/myScript.js
+++ Week 3/Day 1/myScript.js
@@ -40,6 +40,75 @@
 
 // ----------- Array Buffer Example ------------
 
+// let ab = new ArrayBuffer(16);
+// console.log(ab.byteLength);
 
-let ab = new ArrayBuffer(16);
-console.log(ab.byteLength);
+// ------------ Array Buffer Using View Object -------------
+
+
+// let ab = new ArrayBuffer(16);
+
+// let view  = new Uint32Array(ab);
+
+// let view2  = new Uint8Array(ab);
+
+// let view3 = new Uint32Array(ab);
+
+// console.log(view.BYTES_PER_ELEMENT);
+
+// console.log(view2.BYTES_PER_ELEMENT);
+
+// console.log(view3.BYTES_PER_ELEMENT);
+
+// console.log(view.length);
+
+// console.log(view.byteLength);
+
+// view[0] = 1234;
+
+// for(let num of view){
+//     console.log(num);
+// }
+
+
+// ----------- Text Decoder Example ----------
+
+// let uint8Array = new Uint8Array([72, 101, 108, 108, 111]);
+
+// console.log( new TextDecoder().decode(uint8Array) );
+
+// ------------ Text Encoder Example ------------
+
+// let encoder = new TextEncoder();
+
+// let uint8Array = encoder.encode("Hello");
+// console.log(uint8Array);
+
+
+// ---------- Blob Example ----------
+
+let img = document.querySelector('img');
+
+// make <canvas> of the same size
+let canvas = document.createElement('canvas');
+canvas.width = img.clientWidth;
+canvas.height = img.clientHeight;
+
+let context = canvas.getContext('2d');
+
+// copy image to it (this method allows to cut image)
+context.drawImage(img, 0, 0);
+// we can context.rotate(), and do many other things on canvas
+
+// toBlob is async operation, callback is called when done
+canvas.toBlob(function(blob) {
+  // blob ready, download it
+  let link = document.createElement('a');
+  link.download = 'example.png';
+
+  link.href = URL.createObjectURL(blob);
+  link.click();
+
+  // delete the internal blob reference, to let the browser clear memory from it
+  URL.revokeObjectURL(link.href);
+}, 'image/png');
